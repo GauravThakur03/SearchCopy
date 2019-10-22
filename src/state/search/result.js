88cca9ad4857d9912bf53aae7b53aea3ee50c2ef
GP97135 <patidargaurav@johndeere.com>
2019-10-22 11:40:17 +0530

    removed last-modified date from redux

--- src/state/search/result.js
+++ src/state/search/result.js
@@ -3,7 +3,6 @@ import t from 'tcomb';
 const Result = t.struct({
     countrySite: t.maybe(t.String),
     description: t.maybe(t.String),
-    lastUpdated: t.maybe(t.String),
     snippet: t.maybe(t.String),
     title: t.maybe(t.String),
     url: t.maybe(t.String),
--- src/utils/util.js
+++ src/utils/util.js
@@ -51,7 +51,7 @@ function compare(a1, b1, labelA, labelB) {
 export function generateSearchList(document) {
     const list = Array.isArray(document) ? document : [document];
     const results = [];
-    const match = ['last_updated', 'title', 'year', 'snippet', 'url', 'description', 'country-site'];
+    const match = ['title', 'year', 'snippet', 'url', 'description', 'country-site'];
 
     list.map((doc) => {
         const temp = [];
@@ -120,7 +120,7 @@ export function buildQuery(filters) {
 export function buildQueryString(search) {
     const binningValues = Object.values(search.binning.appliedFilters);
 
-    binningValues.push('model-year==Attachments');
+    binningValues.push('model-year==All');
     const binningParam = buildQuery(binningValues);
     const binning = {
         'binning-state': binningParam
