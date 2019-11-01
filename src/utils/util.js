d4aafd569c18677eab50521590466e1d3584a653
Patidar Gaurav <PatidarGaurav@JohnDeere.com>
2019-11-01 14:48:20 +0530

    sort binnings and added content-type=text/xml in query

--- src/utils/util.js
+++ src/utils/util.js
@@ -97,6 +97,37 @@ export function sortModel(bins) {
     });
 }
 
+export const sortBy = (key, reverse) => {
+
+  // Move smaller items towards the front
+  // or back of the array depending on if
+  // we want to sort the array in reverse
+  // order or not.
+  const moveSmaller = reverse ? 1 : -1;
+
+  // Move larger items towards the front
+  // or back of the array depending on if
+  // we want to sort the array in reverse
+  // order or not.
+  const moveLarger = reverse ? -1 : 1;
+
+  /**
+   * @param  {*} a
+   * @param  {*} b
+   * @return {Number}
+   */
+  return (a, b) => {
+    if (a[key] < b[key]) {
+      return moveSmaller;
+    }
+    if (a[key] > b[key]) {
+      return moveLarger;
+    }
+    return 0;
+  };
+}; 
+ 
+
 export function refactorKeys(binningSet) {
     return recursiveKeyModifier(binningSet);
 }
@@ -135,7 +166,8 @@ export function buildQueryString(search) {
     binningValues.push('model-year==All');
     const binningParam = buildQuery(binningValues);
     const binning = {
-        'binning-state': binningParam
+        'binning-state': binningParam,
+        'content-type': 'text/xml'
     };
     const params = {
         ...search.urlParams,
