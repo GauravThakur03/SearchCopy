dadf9648fa583090de791576b06f6490f4357608
GP97135 <patidargaurav@johndeere.com>
2020-03-18 20:13:59 +0530

    Updated the header, footer and setting XML path to relative in configuration file.

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
-export const headerURL = 'https://gsmpreview.tal.deere.com/sales/salesmanual/ssi';
-export const footerURL = 'https://gsmpreview.tal.deere.com/sales/salesmanual/ssi';
-export const searchSettingXmlURL = 'https://gsmpreview.tal.deere.com/data/sales/salesmanual/templatedata/salesmanual/search_settings/data/';
+export const headerURL = '/sales/salesmanual/ssi';
+export const footerURL = '/sales/salesmanual/ssi';
+export const searchSettingXmlURL = '/data/sales/salesmanual/templatedata/salesmanual/search_settings/data/';
 export const autoSuggestionsURL = 'https://servicesextcert.deere.com:443/wex-autocomplete?num=12&filter=&dictionary=deere-vsn-dictionary-autocomplete';
