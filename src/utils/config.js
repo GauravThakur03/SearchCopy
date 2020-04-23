b837b39bf39895e0760d39e4d82ee3ef6702f086
GP97135 <patidargaurav@johndeere.com>
2020-04-23 20:12:42 +0530

    remove prop-types dev build

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
 export const headerURL = '/sales/salesmanual/ssi';
 export const footerURL = '/sales/salesmanual/ssi';
 export const searchSettingXmlURL = '/data/sales/salesmanual/templatedata/salesmanual/search_settings/data/';
-export const autoSuggestionsURL = 'https://servicesext.deere.com:443/wex-autocomplete?num=12&filter=&dictionary=deere-vsn-dictionary-autocomplete';
+export const autoSuggestionsURL = 'https://servicesext.tal.deere.com:443/wex-autocomplete?num=12&filter=&dictionary=deere-vsn-dictionary-autocomplete';
