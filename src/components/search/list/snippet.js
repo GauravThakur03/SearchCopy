90351ced3f8235644b38e1913d3e8069a3cde049
GP97135 <patidargaurav@johndeere.com>
2020-04-23 19:08:04 +0530

    revert ssi chnages for snippet.

--- src/components/search/list/snippet.js
+++ src/components/search/list/snippet.js
@@ -5,11 +5,9 @@ import ResultDiscription from './result-description';
 import ResultAdditionalInfo from './result-additional-info';
 
 const Snippet = ({list}) => {
-    const showDiscription = list.url.indexOf('/shells/') === -1;
-
     return (
         <div className='ns-snippet'>
-            {showDiscription && <ResultDiscription list={list}/>}
+            <ResultDiscription list={list}/>
             <ResultAdditionalInfo
                 list={list}
                 paddingCondition={showDiscription}
